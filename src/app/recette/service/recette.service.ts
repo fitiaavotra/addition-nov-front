83acc3ceaaf9175aa953ab291d9a577614546e4a
fitia <fitiamandimbisoa@gmail.com>
2020-05-23 20:34:53 +0300

    adding endpoint parametre

--- src/app/recette/service/recette.service.ts
+++ src/app/recette/service/recette.service.ts
@@ -3,29 +3,32 @@ import {Injectable} from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import {environment} from './../../../environments/environment'
 
 @Injectable()
 export class RecetteService {
 
     http:HttpClient;
+    EndPointUrl:string;
 
     constructor(http:HttpClient){
         this.http = http;
+        this.EndPointUrl = environment.EndPoint;
     }
 
     GetAll(): Observable<Recette[]> {
-        return this.http.get<Recette[]>('/api/recettes');
+        return this.http.get<Recette[]>(this.EndPointUrl+'/api/Recette');
     }
 
     CreateRecette(recette:Recette):Observable<Recette>{
-        return this.http.post<Recette>('/api/recettes/', recette);
+        return this.http.post<Recette>(this.EndPointUrl+'/api/Recette/', recette);
     }
 
     deleteRecette(Id: string): Observable<any> {
-        return this.http.delete('/api/recettes/' + Id);
+        return this.http.delete(this.EndPointUrl+'/api/Recette/' + Id);
     }
     
     updateRecette(Id: string | number, changes: Partial<Recette>): Observable<any> {
-        return this.http.put('/api/recettes/' + Id, changes);
+        return this.http.put(this.EndPointUrl+'/api/Recette/' + Id, changes);
     }
 }
