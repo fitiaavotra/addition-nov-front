ff49696155ef19c3ca72feab5e9f55f5185e3e5d
fitia <fitiamandimbisoa@gmail.com>
2020-05-24 15:52:01 +0300

    adding sum and percentage

--- src/app/recette/component/recette-addition/recette-addition.component.ts
+++ src/app/recette/component/recette-addition/recette-addition.component.ts
@@ -8,6 +8,7 @@ import { RecetteService } from './../../service/recette.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, NgForm,FormArray } from '@angular/forms';
 import { Update } from '@ngrx/entity';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 
 @Component({
   selector: 'app-recette-list',
@@ -33,6 +34,23 @@ export class RecetteAdditionComponent implements OnInit {
     this.myForm = this.fb.group({
         boisson:this.fb.array([]),
         ingredient:this.fb.array([]),
+        sumBoisson:[{ value: '', disabled: true }],
+        sumCommande:[{ value: '', disabled: true }]
+    })
+
+
+    this.BoissonForms.valueChanges.subscribe(value => {
+        console.log(value);
+        let sum : number = value.map(a=>a.Prix).reduce(function (a,b) {return a+b})
+        if(sum>=100000){
+            sum = sum - ((sum*20)/100);
+        }
+        this.myForm.get('sumBoisson').setValue(sum);
+       });
+
+    this.IngredientForms.valueChanges.subscribe(value=>{
+        let sum : number = value.map(a=>a.Prix).reduce(function (a,b) {return a+b})
+        this.myForm.get('sumCommande').setValue((this.myForm.get('sumBoisson').value+sum))
     })
   }
 
@@ -50,10 +68,10 @@ export class RecetteAdditionComponent implements OnInit {
     this.ingredient = this.service.GetDetail(Id);
     this.IngredientForms.clear();
     this.ingredient.subscribe(value=>value.forEach((lambda)=>{
-        console.log(this.fb)
+        //console.log(this.fb)
 
         const ingredient = this.fb.group({
-            Designation:lambda.name,
+            Designation:[{ value: lambda.name, disabled: true }],
             Prix:[]
         })
         this.IngredientForms.push(ingredient);
